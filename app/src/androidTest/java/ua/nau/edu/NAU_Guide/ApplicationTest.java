81bb1850f843a51f98acbbd5a7de335cf6b08a3d
PIBODIBU <cup.cake@inbox.ru>
2015-09-12 19:27:59 +0300

    Navigation Drawer & Search

---
+++ app/src/androidTest/java/ua/nau/edu/NAU_Guide/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ua.nau.edu.NAU_Guide;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
