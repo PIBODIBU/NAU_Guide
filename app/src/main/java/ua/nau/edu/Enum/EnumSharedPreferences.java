e99815e62f0eb0bd4656fbf3bc7175c555e02adb
Mr.Awesome <cup.cake@inbox.ru>
2015-11-20 04:55:27 +0200

    SVG Parser

---
+++ app/src/main/java/ua/nau/edu/Enum/EnumSharedPreferences.java
@@ -0,0 +1,14 @@
+package ua.nau.edu.Enum;
+
+/**
+ * Created by root on 11/10/15.
+ */
+public enum EnumSharedPreferences {
+    APP_PREFERENCES,
+    SIGNED_IN_KEY,
+    PROFILE_PHOTO_LOCATION_KEY,
+    JUST_SIGNED_KEY,
+    CORP_ID_KEY,
+    CORP_LABEL_KEY,
+
+}
